dd1593374ad6be99b61336307510784a865188d3
Alex Chang <alex.chang@techdesign.com>
2023-04-18 09:22:31 +0800

    List todos

--- support/mp/map.go
+++ support/mp/map.go
@@ -7,6 +7,8 @@ import (
 	"strings"
 )
 
+// Accessible Determine whether the given value is array accessible.
+// todo: check & test cases
 func Accessible(input any) bool {
 	value := reflect.ValueOf(input)
 
@@ -25,6 +27,8 @@ func Accessible(input any) bool {
 	return false
 }
 
+// Add an element to an array using “dot” notation if it doesn't exist.
+// todo: check & test cases
 func Add(input map[any]any, key any, value any) map[any]any {
 	if Get(input, key) == nil {
 		Set(input, key, value)
@@ -32,6 +36,8 @@ func Add(input map[any]any, key any, value any) map[any]any {
 	return input
 }
 
+// Collapse collapses an array of arrays into a single array.
+// todo: check & test cases
 func Collapse(input []any) []any {
 	results := make([]any, 0)
 
@@ -45,6 +51,8 @@ func Collapse(input []any) []any {
 	return results
 }
 
+// CrossJoin returns all possible permutations of the given arrays.
+// todo: check & test cases
 func CrossJoin(arrays ...[]any) [][]any {
 	results := [][]any{{}}
 
@@ -67,6 +75,8 @@ func CrossJoin(arrays ...[]any) [][]any {
 	return results
 }
 
+// Divide an array into two arrays. One with keys and the other with values.
+// todo: check & test cases
 func Divide(input map[any]any) ([]any, []any) {
 	keys := make([]any, 0, len(input))
 	values := make([]any, 0, len(input))
@@ -79,6 +89,142 @@ func Divide(input map[any]any) ([]any, []any) {
 	return keys, values
 }
 
+// Dot returns a flattened associative array with dot notation
+// todo: check & test cases
+func Dot(array map[any]any, prepend string) map[string]any {
+	results := make(map[string]any)
+
+	for key, value := range array {
+		switch v := value.(type) {
+		case map[any]any:
+			if len(v) != 0 {
+				subResults := Dot(v, fmt.Sprintf("%s%v.", prepend, key))
+				for k, val := range subResults {
+					results[k] = val
+				}
+			} else {
+				results[fmt.Sprintf("%s%v", prepend, key)] = value
+			}
+		default:
+			results[fmt.Sprintf("%s%v", prepend, key)] = value
+		}
+	}
+
+	return results
+}
+
+// Undot returns an expanded array from flattened dot notation array
+// todo: check & test cases
+func Undot(array map[string]any) map[any]any {
+	results := make(map[any]any)
+
+	for key, value := range array {
+		Set(results, key, value)
+	}
+
+	return results
+}
+
+// Except returns all the given array except for a specified array of keys.
+// todo: check & test cases
+func Except(array map[any]any, keys []any) map[any]any {
+	for _, key := range keys {
+		delete(array, key)
+	}
+
+	return array
+}
+
+// Exists determines if the given key exists in the provided array.
+// todo: check & test cases
+func Exists(array map[any]any, key any) bool {
+	_, exists := array[key]
+	return exists
+}
+
+// First returns the first element in an array that passes a given truth test.
+// todo: check & test cases
+func First(array map[any]any, callback func(any, any) bool, defaultValue any) any {
+	if callback == nil {
+		if len(array) == 0 {
+			return defaultValue
+		}
+
+		for _, value := range array {
+			return value
+		}
+	}
+
+	for key, value := range array {
+		if callback(value, key) {
+			return value
+		}
+	}
+
+	return defaultValue
+}
+
+// Last returns the last element in an array passing a given truth test.
+// todo: check & test cases
+func Last(array map[any]any, callback func(any, any) bool, defaultValue any) any {
+	if callback == nil {
+		if len(array) == 0 {
+			return defaultValue
+		}
+
+		var lastValue any
+		for _, value := range array {
+			lastValue = value
+		}
+		return lastValue
+	}
+
+	reversedArray := make(map[any]any)
+	for key, value := range array {
+		reversedArray[key] = value
+	}
+
+	return First(reversedArray, callback, defaultValue)
+}
+
+// Flatten flattens a multi-dimensional array into a single level.
+// todo: check & test cases
+func Flatten(array map[any]any, depth int) []any {
+	result := []any{}
+
+	for _, item := range array {
+		subArray, ok := item.(map[any]any)
+		if !ok {
+			result = append(result, item)
+		} else {
+			values := make([]any, 0)
+			if depth == 1 {
+				for _, v := range subArray {
+					values = append(values, v)
+				}
+			} else {
+				values = Flatten(subArray, depth-1)
+			}
+
+			for _, value := range values {
+				result = append(result, value)
+			}
+		}
+	}
+
+	return result
+}
+
+// Forget Remove one or many array items from a given array.
+// todo: check & test cases
+func Forget(array map[any]any, keys []any) {
+	for _, key := range keys {
+		delete(array, key)
+	}
+}
+
+// Get an item from an array using int key.
+// todo: check & test cases
 func Get(array map[any]any, key any, defaultValue ...any) any {
 	if key == nil {
 		return array
@@ -111,124 +257,8 @@ func Get(array map[any]any, key any, defaultValue ...any) any {
 	return array
 }
 
-func Dot(array map[any]any, prepend string) map[string]any {
-	results := make(map[string]any)
-
-	for key, value := range array {
-		switch v := value.(type) {
-		case map[any]any:
-			if len(v) != 0 {
-				subResults := Dot(v, fmt.Sprintf("%s%v.", prepend, key))
-				for k, val := range subResults {
-					results[k] = val
-				}
-			} else {
-				results[fmt.Sprintf("%s%v", prepend, key)] = value
-			}
-		default:
-			results[fmt.Sprintf("%s%v", prepend, key)] = value
-		}
-	}
-
-	return results
-}
-
-func Undot(array map[string]any) map[any]any {
-	results := make(map[any]any)
-
-	for key, value := range array {
-		Set(results, key, value)
-	}
-
-	return results
-}
-
-func Except(array map[any]any, keys []any) map[any]any {
-	for _, key := range keys {
-		delete(array, key)
-	}
-
-	return array
-}
-
-func Exists(array map[any]any, key any) bool {
-	_, exists := array[key]
-	return exists
-}
-
-func First(array map[any]any, callback func(any, any) bool, defaultValue any) any {
-	if callback == nil {
-		if len(array) == 0 {
-			return defaultValue
-		}
-
-		for _, value := range array {
-			return value
-		}
-	}
-
-	for key, value := range array {
-		if callback(value, key) {
-			return value
-		}
-	}
-
-	return defaultValue
-}
-
-func Last(array map[any]any, callback func(any, any) bool, defaultValue any) any {
-	if callback == nil {
-		if len(array) == 0 {
-			return defaultValue
-		}
-
-		var lastValue any
-		for _, value := range array {
-			lastValue = value
-		}
-		return lastValue
-	}
-
-	reversedArray := make(map[any]any)
-	for key, value := range array {
-		reversedArray[key] = value
-	}
-
-	return First(reversedArray, callback, defaultValue)
-}
-
-func Flatten(array map[any]any, depth int) []any {
-	result := []any{}
-
-	for _, item := range array {
-		subArray, ok := item.(map[any]any)
-		if !ok {
-			result = append(result, item)
-		} else {
-			values := make([]any, 0)
-			if depth == 1 {
-				for _, v := range subArray {
-					values = append(values, v)
-				}
-			} else {
-				values = Flatten(subArray, depth-1)
-			}
-
-			for _, value := range values {
-				result = append(result, value)
-			}
-		}
-	}
-
-	return result
-}
-
-func Forget(array map[any]any, keys []any) {
-	for _, key := range keys {
-		delete(array, key)
-	}
-}
-
+// Has checks if an item or items exist in an array using "dot" notation.
+// todo: check & test cases
 func Has(array map[any]any, keys []any) bool {
 	if len(array) == 0 || len(keys) == 0 {
 		return false
@@ -252,6 +282,8 @@ func Has(array map[any]any, keys []any) bool {
 	return true
 }
 
+// HasAny Determine if any of the keys exist in an array using int key
+// todo: check & test cases
 func HasAny(array map[any]any, keys []any) bool {
 	if len(keys) == 0 {
 		return false
@@ -266,6 +298,8 @@ func HasAny(array map[any]any, keys []any) bool {
 	return false
 }
 
+// IsAssoc Determines if an array is associative.
+// todo: finish & test cases
 //func IsAssoc(array map[any]any) bool {
 //	for i, key := range getSortedKeys(array) {
 //		if i != key {
@@ -275,10 +309,14 @@ func HasAny(array map[any]any, keys []any) bool {
 //	return false
 //}
 
+// IsList Determines if an array is a list.
+// todo: finish & test cases
 //func IsList(array map[any]any) bool {
 //	return !IsAssoc(array)
 //}
 
+// Join concatenates elements of a slice into a string with a specified delimiter and final separator
+// todo: finish & test cases
 //func Join(array []any, glue string, finalGlue string) string {
 //	if finalGlue == "" {
 //		return strings.Join(toStringArray(array), glue)
@@ -297,6 +335,8 @@ func HasAny(array map[any]any, keys []any) bool {
 //	return strings.Join(toStringArray(array), glue) + finalGlue + fmt.Sprintf("%v", finalItem)
 //}
 
+// KeyBy Key an associative array by a field or using a callback.
+// todo: check & test cases
 func KeyBy(array map[any]any, keyBy func(any) any) map[any]any {
 	result := make(map[any]any)
 
@@ -308,6 +348,8 @@ func KeyBy(array map[any]any, keyBy func(any) any) map[any]any {
 	return result
 }
 
+// PrependKeysWith Prepend the key names of an associative array.
+// todo: check & test cases
 func PrependKeysWith(array map[any]any, prependWith string) map[any]any {
 	result := make(map[any]any)
 
@@ -319,6 +361,8 @@ func PrependKeysWith(array map[any]any, prependWith string) map[any]any {
 	return result
 }
 
+// Only returns a subset of the items from the given map with specified keys.
+// todo: check & test cases
 func Only(array map[any]any, keys []any) map[any]any {
 	result := make(map[any]any)
 
@@ -331,12 +375,50 @@ func Only(array map[any]any, keys []any) map[any]any {
 	return result
 }
 
+// Pluck an array of values from an array.
+// todo: finish & test cases
+//func Pluck[T any](array []T, value T, key ...int) ([]T, error) {
+//}
+
+// ExplodePluckParameters Explode the "value" and "key" arguments passed to "pluck".
+// todo: finish & test cases
+//func ExplodePluckParameters[T any](array []T, key ...int) ([]T, error) {
+//}
+
+// Map Run a map over each of the items in the array.
+// todo: finish & test cases
+//func Map[T, U any](arr []T, fn func(T, int) U) []U {
+//}
+
 // Prepend the given value to the beginning of an array or associative array.
+// todo: check & test cases
 func Prepend[K comparable, V any](arr map[K]V, value V, key K) map[K]V {
 	arr[key] = value
 	return arr
 }
 
+// Pull Get a value from the array, and remove it.
+// todo: finish & test cases
+//func Pull[T any](arr []T, key int, def T) ([]T, T, error) {
+//}
+
+// Query converts a map into a query string.
+// todo: check & test cases
+func Query(arr map[string]string) string {
+	values := url.Values{}
+	for key, value := range arr {
+		values.Add(key, value)
+	}
+	return values.Encode()
+}
+
+// Random returns one or a specified number of random values from a slice.
+// todo: finish & test cases
+//func Random[T any](arr []T, number *int) ([]T, error) {
+//}
+
+// Set an map item to a given value using int key
+// todo: check & test cases
 func Set(array map[any]any, key any, value any) map[any]any {
 	if key == nil {
 		return map[any]any{
@@ -367,7 +449,28 @@ func Set(array map[any]any, key any, value any) map[any]any {
 	return array
 }
 
+// Shuffle the given array and return the result.
+// todo: finish & test cases
+//func Shuffle[T any](arr []T, seed *int64) []T {
+//}
+
+// Sort the nested array using the given callback.
+// todo: finish & test cases
+//func Sort(arr []any, fn func(i, j int) bool) []any {
+//}
+
+// Sort the nested array in descending order using the given callback.
+// todo: finish & test cases
+//func SortDesc(arr []any, fn func(i, j int) bool) []any {
+//}
+
+// SortRecursive Recursively sort an array by values.
+// todo: finish & test cases
+//func SortRecursive(arr []any, descending bool) ([]any, error) {
+//}
+
 // ToCssClasses Convert a map of string-bool pairs to a string of CSS classes.
+// todo: check & test cases
 func ToCssClasses(mp map[any]bool) string {
 	var classList []string
 
@@ -381,6 +484,7 @@ func ToCssClasses(mp map[any]bool) string {
 }
 
 // ToCssStyles Convert a map of string-bool pairs to a string of CSS styles.
+// todo: check & test cases
 func ToCssStyles[T comparable](mp map[T]bool) string {
 	//func ToCssStyles(mp map[any]bool) string {
 	var styleStrings []string
@@ -394,11 +498,17 @@ func ToCssStyles[T comparable](mp map[T]bool) string {
 	return strings.Join(styleStrings, "; ")
 }
 
-// Query converts an array (map) into a query string.
-func Query(arr map[string]string) string {
-	values := url.Values{}
-	for key, value := range arr {
-		values.Add(key, value)
-	}
-	return values.Encode()
-}
+// Where Filter the array using the given callback.
+// todo: finish & test cases
+//func Where[T any](arr []T, fn func(T) bool) []T {
+//}
+
+// WhereNotNull Filter items where the value is not null.
+// todo: finish & test cases
+//func WhereNotNull[T any](arr []T) []T {
+//}
+
+// Wrap If the given value is not an array and not null, wrap it in one.
+// todo: finish & test cases
+//func Wrap(value any) []any {
+//}
--- support/mp/map_test.go
+++ support/mp/map_test.go
@@ -5,8 +5,111 @@ import (
 	"testing"
 )
 
-func TestToCssClasses(t *testing.T) {
+func TestAccessible(t *testing.T) {
+}
 
+func TestAdd(t *testing.T) {
+}
+
+func TestCollapse(t *testing.T) {
+}
+
+func TestCrossJoin(t *testing.T) {
+}
+
+func TestDivide(t *testing.T) {
+}
+
+func TestDot(t *testing.T) {
+}
+
+func TestUndot(t *testing.T) {
+}
+
+func TestExcept(t *testing.T) {
+}
+
+func TestExists(t *testing.T) {
+}
+
+func TestFirst(t *testing.T) {
+}
+
+func TestLast(t *testing.T) {
+}
+
+func TestFlatten(t *testing.T) {
+}
+
+func TestForget(t *testing.T) {
+}
+
+func TestGet(t *testing.T) {
+}
+
+func TestHas(t *testing.T) {
+}
+
+func TestHasAny(t *testing.T) {
+}
+
+func TestJoin(t *testing.T) {
+}
+
+func TestKeyBy(t *testing.T) {
+}
+
+func TestPrependKeysWith(t *testing.T) {
+}
+
+func TestOnly(t *testing.T) {
+}
+
+func TestMap(t *testing.T) {
+}
+
+func TestPrepend(t *testing.T) {
+}
+
+func TestPull(t *testing.T) {
+}
+
+func TestQuery(t *testing.T) {
+	// Test case 1: Normal input
+	arr := map[string]string{
+		"key1": "value1",
+		"key2": "value2",
+	}
+	expected := "key1=value1&key2=value2"
+	result := Query(arr)
+	assert.Equal(t, expected, result)
+
+	// Test case 2: Empty input
+	arr = map[string]string{}
+	expected = ""
+	result = Query(arr)
+	assert.Equal(t, expected, result)
+}
+
+func TestRandom(t *testing.T) {
+}
+
+func TestSet(t *testing.T) {
+}
+
+func TestShuffle(t *testing.T) {
+}
+
+func TestSort(t *testing.T) {
+}
+
+func TestSortDesc(t *testing.T) {
+}
+
+func TestSortRecursive(t *testing.T) {
+}
+
+func TestToCssClasses(t *testing.T) {
 	classes := ToCssClasses(map[interface{}]bool{"font-bold": true, "mt-4": true, "ml-2": true, "mr-2": false})
 	expected := "font-bold mt-4 ml-2"
 	assert.Equal(t, expected, classes)
@@ -26,36 +129,11 @@ func TestToCssStyles(t *testing.T) {
 	}
 }
 
-func TestPrepend(t *testing.T) {
-	//arr1 := []string{"one", "two", "three", "four"}
-	//result1 := Prepend(arr1, "zero")
-	//expected1 := []string{"zero", "one", "two", "three", "four"}
-	//assert.Equal(t, expected1, result1)
-
-	arr2 := map[string]int{"one": 1, "two": 2}
-	result2 := Prepend(arr2, 0, "zero")
-	expected2 := map[string]int{"zero": 0, "one": 1, "two": 2}
-	assert.Equal(t, expected2, result2)
-
-	//arr3 := map[string]int{"one": 1, "two": 2}
-	//result3 := Prepend(arr3, 0, nil)
-	//expected3 := map[interface{}]int{nil: 0, "one": 1, "two": 2}
-	//assert.Equal(t, expected3, result3)
+func TestWhere(t *testing.T) {
 }
 
-func TestQuery(t *testing.T) {
-	// Test case 1: Normal input
-	arr := map[string]string{
-		"key1": "value1",
-		"key2": "value2",
-	}
-	expected := "key1=value1&key2=value2"
-	result := Query(arr)
-	assert.Equal(t, expected, result)
+func TestWhereNotNull(t *testing.T) {
+}
 
-	// Test case 2: Empty input
-	arr = map[string]string{}
-	expected = ""
-	result = Query(arr)
-	assert.Equal(t, expected, result)
+func TestWrap(t *testing.T) {
 }
